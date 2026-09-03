5e1c3fe5bf73cbc097019e0d286f625a7e16110f
Daniel Mazzeu <danielmazzeu.dev@gmail.com>
2026-09-03 13:28:19 -0300

    Taverna: zoom na caneca dos cards da mesa.

--- src/views/components/tavern-mug-art.tsx
+++ src/views/components/tavern-mug-art.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { TAVERN_MUG_PATH } from "@/shared/constants/site";
 import { cn } from "@/shared/utils/class-names";
+import { IconArt } from "./icon-frame";
 
 export function TavernMugArt({
   unread = 0,
@@ -9,16 +12,13 @@ export function TavernMugArt({
   seated?: boolean;
 }) {
   return (
-    <span className="relative flex h-full w-full items-center justify-center">
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        src={TAVERN_MUG_PATH}
-        alt=""
-        className={cn(
-          "max-h-[92%] max-w-[92%] object-contain",
-          seated && "drop-shadow-[0_0_14px_rgba(224,141,53,0.5)]",
-        )}
-      />
+    <span
+      className={cn(
+        "relative flex h-full w-full items-center justify-center",
+        seated && "drop-shadow-[0_0_14px_rgba(224,141,53,0.5)]",
+      )}
+    >
+      <IconArt source={TAVERN_MUG_PATH} fit="contain" padded={false} inset="max-h-[92%] max-w-[92%]" />
       {unread > 0 ? (
         <span className="absolute -right-0.5 -top-0.5 inline-flex h-5 min-w-5 items-center justify-center rounded-md border border-ember bg-ember px-1 font-mono text-[10px] font-bold tracking-normal text-base">
           {unread > 9 ? "9+" : unread}
