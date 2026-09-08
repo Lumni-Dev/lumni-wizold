7e6d805221a8eedd9a71e35c180896a36b64344e
Daniel Mazzeu <danielmazzeu.dev@gmail.com>
2026-09-08 15:48:46 -0300

    Lower empty equipment slot art opacity for more transparency.

--- src/views/components/item-icon.tsx
+++ src/views/components/item-icon.tsx
@@ -105,7 +105,7 @@ export function EmptySlotArt({
   }
 
   return (
-    <span className="relative flex h-full w-full opacity-75">
+    <span className="relative flex h-full w-full opacity-40">
       <IconArt source={source} padded={false} fit="contain" zoom={false} />
     </span>
   );
